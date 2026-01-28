57c34e4e5b9b66518acefbcf6486564fd65497f9
Sergio Cruz <vlad.crz@gmail.com>
2026-01-27 22:52:19 -0600

    Adding caching

--- src/main/java/com/vscg/demoredis/EmployeeService.java
+++ src/main/java/com/vscg/demoredis/EmployeeService.java
@@ -1,6 +1,9 @@
 package com.vscg.demoredis;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,22 +14,43 @@ public class EmployeeService {
     @Autowired
     EmployeeRepository employeeRepository;
 
+    @Cacheable(value = "employees", key = "#employeeId", sync = true)
     public Employee getEmployee(Integer employeeId) {
+        fakeDelay("Fetching employee from the database...");
         return employeeRepository.findById(employeeId)
                 .orElseThrow(() ->
                         new ResourceNotFoundException(
                                 "Employee not found:" + employeeId));
     }
-
+    @Cacheable(value = "employees")
     public List<Employee> getAllEmployees() {
+
+        fakeDelay("Fetching all employees from the database...");
         return employeeRepository.findAll();
     }
-
+    @CachePut(value = "employees", key = "#employee.id")
     public Employee saveEmployee(Employee employee) {
+
+        System.out.println(
+                "Saving employee to the database and updating cache");
         return employeeRepository.save(employee);
     }
-
+    @CacheEvict(value = "employees", allEntries = true)
     public void deleteEmployee(Integer employeeId) {
+        System.out.println(
+                "Removing all employees from the database and cache");
         employeeRepository.deleteById(employeeId);
     }
+
+    public void fakeDelay(String message) {
+        for (int i = 0; i < 5; i++){
+            System.out.println(message);
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
